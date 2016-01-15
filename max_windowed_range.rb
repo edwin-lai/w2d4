cde136a91f10d9fde3a463ef6dfbc4457b6c02bf
edwin-lai <edwinlai@ucla.edu>
2016-01-14 17:08:19 -0800

    completed max window range

--- max_windowed_range.rb
+++ max_windowed_range.rb
@@ -14,12 +14,20 @@ end
 
 def windowed_max_range(arr, width)
   current_max_range = 0
-  (0..(arr.count - width)).each do |idx|
-    window = 
+  window = MinMaxStackQueue.new
+  pos = 0
+  width.times do
+    |idx| window.enqueue(arr[idx])
+    pos += 1
+  end
+  until pos > arr.size
     maximum = window.max
     minimum = window.min
     range = maximum - minimum
     current_max_range = range if current_max_range < range
+    window.dequeue
+    window.enqueue(arr[pos]) unless arr[pos].nil?
+    pos +=1
   end
   current_max_range
 end
